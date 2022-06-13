433b4774bd618ef85ce3f40fcb028811323815a1
Mateo Santiago <95106063+MateoSDB@users.noreply.github.com>
2022-06-12 22:15:18 -0500

    Ejercicio 3 MapReduce

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -1,3 +1,16 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+#! /usr/bin/ python3
+import sys 
+if __name__ == "__main__":
+
+    ##
+    ## itera sobre cada linea de codigo recibida
+    ## a traves del flujo de entrada
+    ##
+    for line in sys.stdin:
+       data=line.split(',')[0]
+       num=int(line.split(",")[1])
+       
+       sys.stdout.write("{},{},{}\n".format(num,data,num))
--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,18 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+#! /usr/bin python3
+
+import sys
+
+#
+# Esta funcion reduce los elementos que tienen la misma clave
+#
+#! /usr/bin/ python3
+if __name__ == '__main__':
+
+  for line in sys.stdin:
+        numero, llave, valor = line.split(",")
+        valor=int(valor)
+
+        sys.stdout.write(llave + "," + str(valor) + "\n")

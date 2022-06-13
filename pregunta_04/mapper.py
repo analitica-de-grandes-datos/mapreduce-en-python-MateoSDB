1b43f8bc81fe9e201391f368955e67e37273f841
Mateo Santiago <95106063+MateoSDB@users.noreply.github.com>
2022-06-12 22:37:39 -0500

    Ejercicio 4 MapReduce

--- pregunta_04/mapper.py
+++ pregunta_04/mapper.py
@@ -1,3 +1,14 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+#! /usr/bin/ python3
+import sys
+if __name__ == "__main__":
+
+    #
+    # itera sobre cada linea de codigo recibida
+    # a traves del flujo de entrada
+    #
+     for line in sys.stdin:
+       data=line [0:].split(' ')
+       sys.stdout.write("{}\t1\n".format(data[0]))

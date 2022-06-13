47068302fce8cd28759ba23cae0c42e1a7a2c8fa
Mateo Santiago <95106063+MateoSDB@users.noreply.github.com>
2022-06-12 22:10:43 -0500

    Ejercicio 1 MapReduce

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,19 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+#! /usr/bin/ python3
+
+#
+# Esta es la funcion que mapea la entrada a parejas (clave, valor)
+#
+import sys
+if __name__ == "__main__":
+
+    #
+    # itera sobre cada linea de codigo recibida
+    # a traves del flujo de entrada
+    #
+    for line in sys.stdin:
+
+         credit = line [0:-1].split(",")
+         sys.stdout.write("{}\t1\n".format(credit[2]))

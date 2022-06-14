99675d072585076b12f54a3a2c07a4746d8aa4b2
Mateo Santiago <95106063+MateoSDB@users.noreply.github.com>
2022-06-14 00:24:30 -0500

    Ejercicio 8 MapReduce

--- pregunta_08/mapper.py
+++ pregunta_08/mapper.py
@@ -1,3 +1,9 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__": 
+ for line in sys.stdin:
+       letter=line.split('   ')[0] 
+       number = int(line.split('   ')[2]) 
+       sys.stdout.write("{}\t{}\t1\n".format(letter,number)) 
--- pregunta_08/reducer.py
+++ pregunta_08/reducer.py
@@ -1,3 +1,30 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+if __name__ == '__main__':
+    curkey = None
+    suma = 0
+    conteo1 = 0
+
+    for line in sys.stdin:
+
+        key, value, conteo = line.split("\t") 
+        value = float(value)
+        conteo = float(conteo) 
+        if key == curkey:
+
+            suma += value
+            conteo1 += conteo
+        else:
+
+            if curkey is not None:
+
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey, suma, float(suma/conteo1)))  
+
+            curkey = key
+            suma = value 
+            conteo1 = conteo 
+            promedio= float(float(value)/float(conteo1)) 
+            
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey, suma, float(suma/conteo1)))

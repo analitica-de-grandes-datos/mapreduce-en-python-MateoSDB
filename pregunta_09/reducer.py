a81acd6f479fbdef8b135669f0c19845a7be662b
Mateo Santiago <95106063+MateoSDB@users.noreply.github.com>
2022-06-14 00:47:32 -0500

    Ejercicio 9  MapReduce

--- pregunta_09/reducer.py
+++ pregunta_09/reducer.py
@@ -1,3 +1,16 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+if __name__ == '__main__':
+    
+ list=[]  
+ for line in sys.stdin:
+    key1, date, value = line.split("\t") 
+    valor=int(value) 
+    list.append([key1, date, valor]) 
+
+ list = sorted(list, key=lambda x: (x[2]))  
+ list= list[0:6] 
+ for line in list: 
+    sys.stdout.write("{}   {}   {}\n".format(line[0], line[1], line[2]))

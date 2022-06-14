a4b27abcf073b41fcd4f5fd0a2aa8a525dab0eab
Mateo Santiago <95106063+MateoSDB@users.noreply.github.com>
2022-06-13 23:53:26 -0500

    Ejercicio 7 MapReduce

--- pregunta_07/mapper.py
+++ pregunta_07/mapper.py
@@ -1,3 +1,12 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+#! /usr/bin python3
+import sys 
+if __name__ == "__main__":
+ for line in sys.stdin:
+           letter = line.split('   ')[0]
+           date = line.split('   ')[1]
+           number = int(line.split('   ')[2])
+           sys.stdout.write("{}\t{}\t{}\n".format(letter,date,number))
+
--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,16 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+#! /usr/bin python3
+import sys
+if __name__ == '__main__':
+
+ campo_lista=[]
+ for line in sys.stdin:
+    marca, date, value = line.split("\t") 
+    value=int(value) 
+    campo_lista.append([marca, date, value]) 
+
+ campo_lista = sorted(campo_lista, key=lambda x: (x[0], x[2])) 
+ for line in campo_lista: 
+    sys.stdout.write(line[0] +"   " + line[1] +"   " + str(line[2]) + "\n")
